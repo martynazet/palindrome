395ec1f49c6cc10251cb2612e48dbc7f8801db67
Martyna Zawór <mart.zaw.95@gmail.com>
2024-01-29 18:18:21 +0100

    Add docstring

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,10 @@
 def palindrome(word):
+    '''
+    Checks if passed argument is a palindrome
+    Argument: word
+    '''
     if word == word[::-1]:
         return True
     else:
         return False
+        

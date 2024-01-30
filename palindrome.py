b37425df11831fea0feaab57032ab2cff9eebf71
Martyna Zawór <mart.zaw.95@gmail.com>
2024-01-30 08:47:02 +0100

    Simplify code

--- palindrome.py
+++ palindrome.py
@@ -3,8 +3,6 @@ def palindrome(word):
     Checks if passed argument is a palindrome
     Argument: word
     '''
-    if word == word[::-1]:
-        return True
-    else:
-        return False
+    return word == word[::-1]
+   
         
